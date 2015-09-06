cbf68a4f22ac18fa8dc9c9848a1611e1279c6c7f
Wayne Luong <wayneluong93@gmail.com>
2015-09-06 18:49:44 +0100

    Visual changes

--- assets/js/jquery.easypiechart.js
+++ assets/js/jquery.easypiechart.js
@@ -2,9 +2,7 @@
  * easyPieChart
  * Lightweight plugin to render simple, animated and retina optimized pie charts
  *
- * @license 
- * @author Robert Fleischmann <rendro87@gmail.com> (http://robert-fleischmann.de)
- * @version 2.1.6
+ * Wayne Luong
  **/
 
 (function(root, factory) {
